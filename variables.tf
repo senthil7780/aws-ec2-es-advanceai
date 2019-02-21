1f06366cacbaf16ca2439898686e3383edc0ef51
Senthil K <senthilkumar.kannappan@gmail.com>
2019-02-21 10:20:25 +0800

    updated commit

--- variables.tf
+++ variables.tf
@@ -2,9 +2,9 @@ variable "aws_region" {
   default     = "ap-southeast-1"
 }
 
-#variable "aws_access_key" {}
+variable "aws_access_key" {}
 
-#variable "aws_secret_key" {}
+variable "aws_secret_key" {}
 
 variable "ami_key_pair_name" {
   default = "mykeypair"
